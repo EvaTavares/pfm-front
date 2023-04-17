f13ef3bb05ef65d03736819fb18fd53331aa5e0d
Eva Tavares <eva.programadora@gmail.com>
2023-04-17 10:50:37 -0300

    feat: add rotas react-router-dom e MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-  return <div className="App">LOGIN</div>;
+  return (
+    <div className="App">
+      <CssBaseline />
+      <AppRoutes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Button>Teste</Button>} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/errandsList" element={<ListaRecados />} /> */}
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
+};
+
+export default AppRoutes;
